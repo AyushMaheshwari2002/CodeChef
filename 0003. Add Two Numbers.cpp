a1d20de6b7d57668af2c598d7dbb72fde7d0c685
AyushMaheshwari2002 <90818051+AyushMaheshwari2002@users.noreply.github.com>
2022-01-23 19:17:17 +0530

    Create 0003. Add Two Numbers.cpp

---
+++ 0003. Add Two Numbers.cpp
@@ -0,0 +1,21 @@
+// The task is very simple: given two integers A and B, write a program to add these two numbers and output it.
+
+
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    int t;
+    cin >> t;
+    
+    while(t--)
+    {
+        int A, B, sum;
+        cin >> A >> B;
+        
+        sum = A + B;
+        cout << sum<< endl;
+    }
+	return 0;
+}
