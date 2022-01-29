1f1f3380428bd5ca0e15e339027a668c28b813a4
AyushMaheshwari2002 <90818051+AyushMaheshwari2002@users.noreply.github.com>
2022-01-29 14:31:59 +0530

    Create 01. Buy Please.cpp

---
+++ DSA Learning Series/1. Easy Problems/01. Buy Please.cpp
@@ -0,0 +1,21 @@
+/*  Link :  https://www.codechef.com/CCSTART2/problems/BUYPLSE
+
+   Chef went to a shop and buys a pens and b pencils. Each pen costs x units and each pencil costs y units. Now find what is the total amount Chef will spend to 
+   buy a pens and b pencils.
+*/
+
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() 
+{	
+    int a, b, x, y;
+    cin >> a >> b >> x >> y;
+    
+    int cost = ((a * x) + (b * y));
+    cout << cost << endl;
+	return 0;
+}
+
