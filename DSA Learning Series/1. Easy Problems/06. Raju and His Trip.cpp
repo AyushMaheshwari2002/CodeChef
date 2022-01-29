9f342da7e16dba43e7d134e3634aa784f59c6cac
AyushMaheshwari2002 <90818051+AyushMaheshwari2002@users.noreply.github.com>
2022-01-29 14:50:37 +0530

    Create 06. Raju and His Trip.cpp

---
+++ DSA Learning Series/1. Easy Problems/06. Raju and His Trip.cpp
@@ -0,0 +1,24 @@
+/*  Link :   https://www.codechef.com/CCSTART2/problems/VALTRI
+
+  Raju is planning to visit his favourite restaurant. He shall travel to it by bus. Only the buses whose numbers are divisible by 5 or by 6 shall take him to his destination. 
+  You are given a bus number N. Find if Raju can take the bus or not. Print YES if he can take the bus, otherwise print NO.
+*/
+
+
+
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    int N;
+    cin >> N;
+    
+    if(N % 5 == 0 || N % 6 == 0)
+        cout << "YES";
+        
+    else
+        cout << "NO";
+	return 0;
+}
+
