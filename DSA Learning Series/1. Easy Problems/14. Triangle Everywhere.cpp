1397e1efe3fe69f5ca2a0e266f835ada2cfa40ad
AyushMaheshwari2002 <90818051+AyushMaheshwari2002@users.noreply.github.com>
2022-02-09 21:10:35 +0530

    Create 14. Triangle Everywhere.cpp

---
+++ DSA Learning Series/1. Easy Problems/14. Triangle Everywhere.cpp
@@ -0,0 +1,48 @@
+/*  Link :   https://www.codechef.com/CCSTART2/problems/EXTRICHK
+
+    You're given the length of three sides a, b, and c respectively. Now If these three sides can form an Equilateral Triangle then print 1, if these three sides 
+    can form an Isosceles Triangle then print 2, if these three sides can form a Scalene Triangle then print 3, otherwise print −1.
+
+                    Sample Input 1:
+                    2 4 3
+                    Sample Output 1:
+                    3
+                    Sample Input 2:
+                    4 4 4
+                    Sample Output 2:
+                    1
+                    Sample Input 3:
+                    4 4 9
+                    Sample Output 3:
+                    -1
+                    EXPLANATION:
+                    In the first example, (2, 4, 3) will form a Scalene Triangle.
+                    In the second example, (4, 4, 4) will form an Equilateral Triangle.
+                    In the third example, (4, 4, 9) will not form a triangle.
+*/
+
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a, b, c;
+    cin >> a >> b >> c;
+    
+    if (a + b > c && b + c > a && c + a > b)
+    {
+        if (a == b && b == c)
+            cout << "1";
+        else if (a == b || b == c || c == a)
+            cout << "2";
+        else
+            cout << "3";
+    }
+    else
+        cout << "-1";
+        
+    return 0;
+}
+
+
