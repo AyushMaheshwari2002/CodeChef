42c1cbb701b93b910d4890ab64163e41c1e48dd8
Ayush Maheshwari <90818051+AyushMaheshwari2002@users.noreply.github.com>
2022-07-25 17:35:24 +0530

    Update 09. Multiple of 3.cpp

--- DSA Learning Series/2. Complexity Analysis + Basics Warm Up/09. Multiple of 3.cpp
+++ DSA Learning Series/2. Complexity Analysis + Basics Warm Up/09. Multiple of 3.cpp
@@ -27,3 +27,57 @@
 
 
 
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+	int t;
+	cin >> t;
+	
+	while(t--)
+	{
+	    long long int k;
+	    cin >> k;
+	    
+	    int d0, d1;
+	    cin >> d0 >> d1;
+	    
+	    int d2 = (d0 + d1) % 10;
+	    int d3 = (2 * d2) % 10;         
+	    /* d3 = (d0 + d1 + d2) % 10
+	       d3 = (d0 + d1 + (d0 + d1) % 10) % 10
+	       d3 = (d0 % 10 + d1 % 10 + d0 % 10 + d1 % 10) % 10
+	       d3 = 2 * (d0 + d1) % 10
+	       d3 = (2 * d2) % 10
+	   */
+	   int d4 = (4 * d2) % 10;
+	   int d5 = (8 * d2) % 10;
+	   int d6 = (6 * d2) % 10;
+	   /* we can't take d7,d8,d9... b'coz it will form loops*/
+	   
+	   /* After that we have count how many times d7,d8,d9,... appears in a number
+	      we will always divide it by 4 b'coz it forms loop every time after finding 4th number */
+	   
+	   long long int k1 = k / 4;                // means d7 placed number appears k/4 times
+	   long long int k2 = (k - 1) / 4;          // means d8 placed number appears k-1/4 times
+	   long long int k3 = (k - 2) / 4;          // means d9 placed number appears k-2/4 times
+	   long long int k4 = (k - 3) / 4;          // means d10 placed number appears k-3/4 times
+	   
+	   /* after that we have to create a number so we can check that it is multiple of 3 or not */
+	   long long sum = d0 + d1 + d2 + d3*k1 + d4*k2 + d5*k3 + d6*k4;
+	   
+	   if(sum % 3 == 0) 
+	    {
+	        cout << "YES" << "\n";
+	    }
+	    else
+	    {
+	        cout << "NO" << "\n";
+	    }
+	}
+	return 0;
+}
+
+
+
