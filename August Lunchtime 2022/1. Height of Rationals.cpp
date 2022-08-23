a91b6ea29272fdf6e7cf6f963624883163e09983
Ayush Maheshwari <90818051+AyushMaheshwari2002@users.noreply.github.com>
2022-08-23 22:41:37 +0530

    Create 1. Height of Rationals.cpp

---
+++ August Lunchtime 2022/1. Height of Rationals.cpp
@@ -0,0 +1,20 @@
+/*  Link :   https://www.codechef.com/submit/HEIGHTRATION
+
+    In a recent breakthrough in mathematics, the proof utilized a concept called Height.
+    Consider a fraction a / b. Its Height is defined as the maximum of its numerator and denominator. So, for example, the Height of 3 / 19 would be 19, and the Height
+    of 27 / 4 would be 27.
+    Given a and b, find the Height of a / b.
+    
+    Sample 1:
+              Input :  3 19
+              Output : 19
+              Explanation: The maximum of {3,19} is 19. Hence the Height of 3 / 19 is 19.
+             
+    Sample 2:
+              Input :  27 4
+              Output : 27
+              Explanation: The maximum of {27,4} is 27. Hence the Height of 27 / 4 is 27.
+*/
+
+
+
