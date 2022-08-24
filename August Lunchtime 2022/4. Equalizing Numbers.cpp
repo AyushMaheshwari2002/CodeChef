2e37cebd653d9a4a42decd3612128c40fe40cebb
Ayush Maheshwari <90818051+AyushMaheshwari2002@users.noreply.github.com>
2022-08-24 23:38:48 +0530

    Create 4. Equalizing Numbers.cpp

---
+++ August Lunchtime 2022/4. Equalizing Numbers.cpp
@@ -0,0 +1,23 @@
+/*  Link :     https://www.codechef.com/submit/EQLZING
+
+    Chef has two integers A and B. In one operation he can choose any integer d, and make one of the following two moves :
+             * Add d to A and subtract d from B.
+             * Add d to B and subtract d from A.
+    Chef is allowed to make as many operations as he wants. Can he make A and B equal?
+    
+    Sample 1:
+            Input:
+                  2
+                  3 3
+                  1 2
+            Output :
+                  Yes
+                  No
+            Explanation:
+                  Test case 1: Since A and B are already equal, Chef does not need any operations.
+                  Test case 2: It can be shown that A and B can never be made equal using any number of given operations.
+*/
+
+
+
+
