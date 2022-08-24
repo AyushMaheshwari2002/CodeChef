511edd2d3ec4874d980a69ec5865392382fbfc8f
Ayush Maheshwari <90818051+AyushMaheshwari2002@users.noreply.github.com>
2022-08-24 23:39:10 +0530

    Update 4. Equalizing Numbers.cpp

--- August Lunchtime 2022/4. Equalizing Numbers.cpp
+++ August Lunchtime 2022/4. Equalizing Numbers.cpp
@@ -21,3 +21,31 @@
 
 
 
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+	int t;
+	cin >> t;
+	
+	while(t--)
+	{
+	    int a, b, d;
+	    cin >> a >> b;
+	    
+	    if(abs(a - b) % 2 == 0)
+	    {
+	        cout << "Yes" << endl;
+	    }
+	    else
+	    {
+	        cout << "No" << endl;
+	    }
+	}
+	return 0;
+}
+
+
+
+
