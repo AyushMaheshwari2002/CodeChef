6295692a610256d18021e67d0d69448fed7d143d
Ayush Maheshwari <90818051+AyushMaheshwari2002@users.noreply.github.com>
2022-08-25 23:33:59 +0530

    Create 5. Easy Permutation.cpp

---
+++ August Lunchtime 2022/5. Easy Permutation.cpp
@@ -0,0 +1,22 @@
+/*  Link :    https://www.codechef.com/submit/EASYPERM
+
+    You are given an integer N.
+    Construct any permutation A of N integers such that the value LCM(1 + A1 ,2 + A2, ..., N + AN) is minimised.
+    
+    Note:
+        LCM denotes the least common multiple of the integers.
+        A permutation of N integers contains all integers from 1 to N exactly once.
+        
+    Sample 1:
+            Input :
+                  1
+                  2
+            Output :
+                  2 1
+            Explanation:
+                  Test case 1: The permutation for which the value is minimised is [2,1]. The minimum value is LCM(1+2,2+1) = LCM(3,3) = 3.
+*/
+
+
+
+
