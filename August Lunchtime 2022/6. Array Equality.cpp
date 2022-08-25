4d3292d371a9821f8c6fd7d2f3b7ab27c7d974eb
Ayush Maheshwari <90818051+AyushMaheshwari2002@users.noreply.github.com>
2022-08-25 23:38:27 +0530

    Create 6. Array Equality.cpp

---
+++ August Lunchtime 2022/6. Array Equality.cpp
@@ -0,0 +1,24 @@
+/*  Link :    https://www.codechef.com/submit/ARREQU
+
+    Chef has an array of N integers. Chef can rearrange this array in any manner.
+    Chef doesn’t like the array if any two adjacent elements are equal. Determine whether there exists a rearrangement of the array that Chef likes.
+    
+    Sample 1:
+            Input :
+                  2
+                  5
+                  1 1 1 1 1
+                  3
+                  3 3 4
+            Output :
+                  No
+                  Yes
+            Explanation:
+                  Test case 1: There exists no rearrangement of the array which Chef will like.
+                  Test case 2: A rearrangement of the array that Chef likes is [3,4,3]. Note that in this rearrangement, no two adjacent elements are equal.
+*/
+
+
+
+
+
