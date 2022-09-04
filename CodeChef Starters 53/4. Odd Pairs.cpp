bcfdc85b48c4b459e9209cdc5199b9c3aaa106b1
Ayush Maheshwari <90818051+AyushMaheshwari2002@users.noreply.github.com>
2022-09-04 23:13:03 +0530

    Create 4. Odd Pairs.cpp

---
+++ CodeChef Starters 53/4. Odd Pairs.cpp
@@ -0,0 +1,31 @@
+/*  Link :    https://www.codechef.com/submit/ODDPAIRS
+
+    Given an integer N, determine the number of pairs (A,B) such that:
+            * 1 ≤ A,B ≤ N;
+            * A + B is odd.
+
+    Sample 1:
+          Input :
+              5
+              1
+              2
+              3
+              100
+              199
+              
+          Output :
+              0
+              2
+              4
+              5000
+              19800
+              
+          Explanation:
+              Test case 1: There are no pairs satisfying the given conditions.
+              Test case 2: The pairs satisfying both conditions are: (1,2) and (2,1).
+              Test case 3: The pairs satisfying both conditions are: (1,2),(2,1),(2,3), and (3,2).
+*/
+
+
+
+
