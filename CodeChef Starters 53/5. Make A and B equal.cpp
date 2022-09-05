ed7209426bd4c85063bc6b31f4a83075ca5cc0a7
Ayush Maheshwari <90818051+AyushMaheshwari2002@users.noreply.github.com>
2022-09-05 23:07:58 +0530

    Create 5. Make A and B equal.cpp

---
+++ CodeChef Starters 53/5. Make A and B equal.cpp
@@ -0,0 +1,40 @@
+/*  Link :    https://www.codechef.com/submit/MAKEABEQUAL
+
+    Chef is given two arrays A and B of length N each.
+    In one operation Chef can choose one element of A and one element of B and increase them by 1.
+    More formally: Chef can pick two integers i,j (1≤i,j≤N) and increment Ai and Bj by 1.
+    Determine the minimum number of operations required to make A and B equal.
+    Output -1 if it is not possible to make A and B equal.
+    
+    Sample 1:
+          Input :
+              3
+              2
+              1 2
+              2 1
+              3
+              1 1 2
+              2 2 1
+              3
+              4 6 8
+              5 7 6
+              
+         Output :
+              1
+              -1
+              2
+              
+        Explanation:
+              Test case 1: We can choose i = 1 and j = 2 and increment Ai and Bj by 1. Thus, both arrays become [2,2] and are equal. We require only 1 operation to 
+                           make these arrays equal. It can be proven that the arrays cannot be made equal in less than one operation.
+              Test case 2: Both the arrays cannot be made equal using any number of operations.
+              Test case 3: We perform 2 operations as follows:
+                           * Choose i = 1, j = 3: The arrays become A = [5, 6, 8] and B = [5, 7, 7].
+                           * Choose i = 2, j = 3: The arrays become A = [5, 7, 8] and B = [5, 7, 8].
+                            Thus, both arrays can be made equal using 2 operations.
+*/
+
+
+
+
+
