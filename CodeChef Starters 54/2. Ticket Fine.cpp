aba4af3400d4114035cf1d85eeadc38849c7b61f
Ayush Maheshwari <90818051+AyushMaheshwari2002@users.noreply.github.com>
2022-09-06 23:06:04 +0530

    Create 2. Ticket Fine.cpp

---
+++ CodeChef Starters 54/2. Ticket Fine.cpp
@@ -0,0 +1,28 @@
+/*  Link :    https://www.codechef.com/submit/TCKTFINE
+
+    On a certain train, Chef-the ticket collector, collects a fine of Rs. X if a passenger is travelling without a ticket. It is known that a passenger carries either
+    a single ticket or no ticket.
+    P passengers are travelling and they have a total of Q tickets. Help Chef calculate the total fine collected.
+    
+    Sample 1:
+         Input :
+              4
+              4 1 1
+              2 10 7
+              8 5 4
+              9 7 0
+        Output :
+              0
+              6
+              8
+              63
+        Explanation:
+              Test case 1: The total number of passengers travelling without ticket are 1 - 1 = 0. So the total fine collected is 0⋅4 = 0 rupees.
+              Test case 2: The total number of passengers travelling without ticket are 10 - 7 = 3. So the total fine collected is 3⋅2 = 6 rupees.
+              Test case 3: The total number of passengers travelling without ticket are 5 - 4 = 15. So the total fine collected is 1⋅8 = 8 rupees.
+              Test case 4: The total number of passengers travelling without ticket are 7 - 0 = 7. So the total fine collected is 7⋅9 = 63 rupees.
+*/
+
+
+
+
