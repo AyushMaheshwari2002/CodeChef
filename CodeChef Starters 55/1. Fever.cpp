58ab4cc711cf14c712878bb3c67bd70e45a0ccd9
Ayush Maheshwari <90818051+AyushMaheshwari2002@users.noreply.github.com>
2022-09-08 23:47:46 +0530

    Create 1. Fever.cpp

---
+++ CodeChef Starters 55/1. Fever.cpp
@@ -0,0 +1,25 @@
+/*  Link :    https://www.codechef.com/submit/FEVER
+
+    Chef is not feeling well today. He measured his body temperature using a thermometer and it came out to be X °F.
+    A person is said to have fever if his body temperature is strictly greater than 98 °F.
+    Determine if Chef has fever or not.
+    
+    Sample 1:
+          Input :
+                3
+                98
+                100
+                96
+          Output :
+                NO
+                YES
+                NO
+          Explanation:
+                Test Case 1: Since X = 98 is not greater than 98, Chef does not have fever.
+                Test Case 2: Since X = 100 is greater than 98, Chef has fever.
+                Test Case 3: Since X = 96 is not greater than 98, Chef does not have fever.
+*/
+
+
+
+
