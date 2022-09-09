9523efc1c8942d48bee7bdc565077fb5ef8f4cb1
Ayush Maheshwari <90818051+AyushMaheshwari2002@users.noreply.github.com>
2022-09-09 23:36:05 +0530

    Update 3. Balls and Boxes.cpp

--- CodeChef Starters 55/3. Balls and Boxes.cpp
+++ CodeChef Starters 55/3. Balls and Boxes.cpp
@@ -26,3 +26,33 @@
 
 
 
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int t;
+	cin >> t;
+	
+	while(t--)
+	{
+	    int n, k;
+	    cin >> n >> k;
+	    
+	    long ans = (k*(k+1))/2;
+	    
+	    if(ans <= n)
+	    {
+	        cout << "Yes" << endl;
+	    }
+	    else {
+	        cout << "No" << endl;
+	    }
+	    
+	}
+	return 0;
+}
+
+
+
