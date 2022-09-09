8437711392ef2505e69f42da578b96a2cd9798e2
Ayush Maheshwari <90818051+AyushMaheshwari2002@users.noreply.github.com>
2022-09-09 23:29:47 +0530

    Create 2. Broken Phone.cpp

---
+++ CodeChef Starters 55/2. Broken Phone.cpp
@@ -0,0 +1,24 @@
+/*  Link :    https://www.codechef.com/submit/BROKENPHONE
+
+    Uttu broke his phone. He can get it repaired by spending X rupees or he can buy a new phone by spending Y rupees. Uttu wants to spend as little money as possible.
+    Find out if it is better to get the phone repaired or to buy a new phone.
+    
+    Sample 1:
+        Input :
+              3
+              100 1000
+              10000 5000
+              3000 3000
+        Output :
+              REPAIR
+              NEW PHONE
+              ANY
+        Explanation:
+              Test Case 1: It is better to get the phone repaired since 100 < 1000.
+              Test Case 2: It is better to buy a new phone since 10000 > 5000.
+              Test Case 3: Uttu can choose either of the two options since 3000 = 3000.
+*/
+
+
+
+
