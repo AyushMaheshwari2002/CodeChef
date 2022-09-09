23f0c605294987441fb728a0a35365c63f77fd9b
Ayush Maheshwari <90818051+AyushMaheshwari2002@users.noreply.github.com>
2022-09-09 23:35:18 +0530

    Create 3. Balls and Boxes.cpp

---
+++ CodeChef Starters 55/3. Balls and Boxes.cpp
@@ -0,0 +1,28 @@
+/*  Link :    https://www.codechef.com/submit/BALLBOX
+
+    You have N balls and K boxes. You want to divide the N balls into K boxes such that:
+          * Each box contains ≥ 1 balls.
+          * No two boxes contain the same number of balls.
+    Determine if it is possible to do so.
+
+    Sample 1:
+         Input :
+              4
+              3 4
+              30 3
+              2 2
+              1 1
+        Output :
+              NO
+              YES
+              NO
+              YES
+        Explanation:
+              Test Case 1: It is not possible to divide the 3 balls into 4 boxes such that each box contains ≥ 1 balls.
+              Test Case 2: One way to divide the 30 balls into 3 boxes is the following: [5, 9, 16].
+              Test Case 3: It is not possible to divide the 2 balls into 2 boxes such that no two boxes contain the same number of balls.
+              Test Case 4: We can divide 1 ball into 1 box.
+*/
+
+
+
