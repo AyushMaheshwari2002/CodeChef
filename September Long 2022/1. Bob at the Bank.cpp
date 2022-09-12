5deed9ce0f4db0545d38c5892779ec854a16f53d
Ayush Maheshwari <90818051+AyushMaheshwari2002@users.noreply.github.com>
2022-09-12 23:04:01 +0530

    Update 1. Bob at the Bank.cpp

--- September Long 2022/1. Bob at the Bank.cpp
+++ September Long 2022/1. Bob at the Bank.cpp
@@ -26,3 +26,23 @@
 
 
 
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >>  t;
+    
+    while(t--)
+    {
+        int w, x, y, z;
+        cin >> w >> x >> y >> z;
+        
+        cout << (w + (x-y) * z) << endl;
+    }
+	return 0;
+}
+
+
+
