a4003eb5f28f080034bb92222fbc8980bd938c66
Ayush Maheshwari <90818051+AyushMaheshwari2002@users.noreply.github.com>
2022-09-12 22:53:16 +0530

    Update 6. OR Tuples.cpp

--- CodeChef Starters 55/6. OR Tuples.cpp
+++ CodeChef Starters 55/6. OR Tuples.cpp
@@ -19,4 +19,33 @@
                 Test case 1: The following 4 triplets (A, B, C) satisfy A|B = 10, B|C = 12, and C|A = 14 : (2, 8, 12), (10, 0, 12), (10, 8, 4) and (10, 8, 12).
                 Test case 2: The following triplet (A, B, C) satisfies A|B = 0, B|C = 5, and C|A = 5 : (0, 0, 5).
                 Test case 3: There are no triplets satisfying all the conditions.
+*/
+
+
+
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+	int t;
+	cin >> t;
+	
+	while(t--)
+	{
+	    long p, q, r;
+	    cin >> p >> q >> r;
+	    
+	    long arr[4] = {1,0,1,4};
+	    long ans = 1;
+	    
+	    for(int i = 0; i < 50; i++)
+	    {
+	        ans *= arr[((p>>i)&1)+((q>>i)&1)+((r>>i)&1)];
+	    }
+	    
+	    cout << ans << endl;
+	}
+	return 0;
+}
 
