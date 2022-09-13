e2fcac2e60c5f3597a00c534e47f3cb1e569c307
Ayush Maheshwari <90818051+AyushMaheshwari2002@users.noreply.github.com>
2022-09-13 23:39:23 +0530

    Update 3. Good Pairs.cpp

--- September Long 2022/3. Good Pairs.cpp
+++ September Long 2022/3. Good Pairs.cpp
@@ -27,7 +27,51 @@
                 Test case 1: No good pair is present.
                 Test case 2: The good pairs (i, j) are: (2,3),(3,4), and (2,4). To elaborate, gcd(A2, A3) = lcm(A2, A3) = 5
                 Test case 3: The good pairs (i,j) are: (1,4),(2,3),(5,6),(6,7), and (5,7). To elaborate, gcd(A1, A4) = lcm(A1, A4) = 2
-                Test case 4: The good pair (i,j) are: (1,2) and (3,4). To elaborate, gcd(A_3, A_4) = lcm(A_3, A_4) = 18
+                Test case 4: The good pair (i,j) are: (1,2) and (3,4). To elaborate, gcd(A3, A4) = lcm(A3, A4) = 18
                 Test case 5: No good pair is present.
 */
+
+
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() 
+{
+    int t;
+    cin >> t;
+    
+    while(t--)
+    {
+        long long n;
+        cin >> n;
+        
+        long long arr[n];
+        for(int i = 0; i < n; i++)
+        {
+            cin >> arr[i];
+        }
+        
+        unordered_map<long,long> m;
+        for(auto i : arr)
+        {
+            m[i]++;
+        }
+        
+        long long ans = 0;
+        for(auto i : m)
+        {
+            if(i.second > 1)
+            {
+                ans += i.second * (i.second - 1)/2;
+            }
+        }
+        
+        cout << ans << endl;
+    }
+	return 0;
+}
+
+
 
