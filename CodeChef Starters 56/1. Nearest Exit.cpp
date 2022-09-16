ddd99e812b93cb27d9526df8130ca0fe77142044
Ayush Maheshwari <90818051+AyushMaheshwari2002@users.noreply.github.com>
2022-09-16 23:34:55 +0530

    Update 1. Nearest Exit.cpp

--- CodeChef Starters 56/1. Nearest Exit.cpp
+++ CodeChef Starters 56/1. Nearest Exit.cpp
@@ -30,8 +30,33 @@
                   Test case 3: The exit is located beside seat 100. Hence, the passenger can take this exit without moving to any other seat.
                   Test case 4: To take exit at seat 1, the passenger needs to move 29 seats. However, to take the exit at seat 100, the passenger needs to move 70 
                                seats. Thus, exit at seat 1 is closer.
-
 */
+
+
+
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+	int t;
+	cin >> t;
+	
+	while(t--)
+	{
+	    int x;
+	    cin >> x;
+	    
+	    if(x <= 50)
+	    {
+	        cout << "LEFT" << endl;
+	    }
+	    else {
+	        cout << "RIGHT" << endl;
+	    }
+	}
+	return 0;
+}
 
 
 
